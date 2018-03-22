0597963b1d675cfc214aac9f1cd8f44ff0e53e4e
s289588 <dante.beltran@ocsbstudent.ca>
2018-03-22 10:40:37 -0400

    added stuff

--- main.lua
+++ main.lua
@@ -23,21 +23,65 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
+local operator
+local counter = 0
+local counterText
 
 -------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 -------------------------------------------------------------------------------------
 
 local function AskQuestion()
-	-- generate 2 random numbers between a max. and a min. number
-	randomNumber1 = math.random(10, 20)
-	randomNumber2 = math.random(10, 20)
 
-	correctAnswer = randomNumber1 + randomNumber2
+	operator = math.random(1,4)
 
-	-- create question in text object
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+		if (operator == 1) then
 
+		-- generate 2 random numbers between a max. and a min. number
+		randomNumber1 = math.random(10, 20)
+		randomNumber2 = math.random(10, 20)
+	
+		correctAnswer = randomNumber1 + randomNumber2
+	
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+	
+
+	elseif (operator == 2) then
+		-- generate 2 random numbers between a max. and a min. number
+		randomNumber1 = math.random(10, 20)
+		randomNumber2 = math.random(10, 20)
+		if (randomNumber2 > randomNumber1) then
+			correctAnswer = randomNumber2 - randomNumber1
+			-- create question in text object
+			questionObject.text = randomNumber2 .. " - " .. randomNumber1 .. " = "
+		else
+			correctAnswer = randomNumber1 - randomNumber2
+			-- create question in text object
+			questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+		end
+
+	elseif (operator == 3) then
+		-- generate 2 random numbers between a max. and a min. number
+		randomNumber1 = math.random(1, 10)
+		randomNumber2 = math.random(1, 10)
+	
+		correctAnswer = randomNumber1 * randomNumber2
+	
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+
+	elseif (operator == 4) then
+		-- generate 2 random numbers between a max. and a min. number
+		randomNumber1 = math.random(1, 10)
+		randomNumber2 = math.random(1, 10)
+	
+		correctAnswer = randomNumber1 * randomNumber2
+	
+		-- create question in text object
+		questionObject.text = correctAnswer .. " / " .. randomNumber2 .. " = "
+		correctAnswer = randomNumber1
+	end
 end
 
 local function  HideCorrect()
@@ -54,9 +98,7 @@ local function NumericFeildListener( event )
 
 	-- User begins editing "numericField" 
 	if ( event.phase == "began" ) then
-
-		-- clear text field
-		event.target.text = ""
+		
 
 	elseif event.phase == "submitted" then
 
@@ -66,10 +108,17 @@ local function NumericFeildListener( event )
 		-- if the users answer and the correct answer are the same:
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
-			timer.performWithDelay(2000, HideCorrect)
+			timer.performWithDelay(3000, HideCorrect)
+			event.target.text = ""
+			counter = counter + 1
+			-- display counter
+			counterText = display.newText("Point Counter = "..counter, display.contentWidth/2, display.contentHeight/5, nil, 50)
+			counterText:setTextColor (190/255, 100/255, 30/255)
+			
 		else
 			incorrectObject.isVisible = true
-			timer.performWithDelay(2000, HideIncorrect)
+			timer.performWithDelay(3000, HideIncorrect) 
+			event.target.text = ""
 
 		end
 	end
@@ -80,7 +129,7 @@ end
 --------------------------------------------------------------------------------------
 
 -- displays a question and sets the color
-questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/3, nil, 60 )
 questionObject:setTextColor(190/255, 100/255, 30/255)
 
 -- create the correct text object and make it invisible
@@ -94,7 +143,7 @@ incorrectObject:setTextColor(190/255, 0/255, 0/255)
 incorrectObject.isVisible = false
 
 -- create numeric field
-numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80)
+numericField = native.newTextField( 550, display.contentHeight/3, 150, 80)
 numericField.inputType = "number"
 
 -- add the event listener for the numeric field
